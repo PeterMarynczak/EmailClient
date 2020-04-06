0c0f0a5dace9c4d852d1964154acb035cbf1c7dc
unknown <piotr.marynczak.programista@gmail.com>
2020-04-06 10:11:31 +0200

    first commit

---
+++ com/marynczak/view/First.java
@@ -0,0 +1,10 @@
+package com.marynczak.view;
+
+import javafx.fxml.FXML;
+
+public class First {
+    @FXML
+    public void buttonAction(){
+        System.out.println("Clicked the Button!");
+    }
+}
---
+++ module-info.java
@@ -0,0 +1,9 @@
+module EmailClient {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.web;
+
+    opens com.marynczak;
+    opens com.marynczak.view;
+}
