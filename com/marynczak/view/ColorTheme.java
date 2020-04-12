41477afd33ec345a7c2768c743b92b35eae05c9a
unknown <piotr.marynczak.programista@gmail.com>
2020-04-12 15:36:04 +0200

    configured options window

---
+++ com/marynczak/view/ColorTheme.java
@@ -0,0 +1,7 @@
+package com.marynczak.view;
+
+public enum ColorTheme {
+    LIGHT,
+    DEFAULT,
+    DARK
+}
---
+++ com/marynczak/view/FontSize.java
@@ -0,0 +1,7 @@
+package com.marynczak.view;
+
+public enum FontSize {
+    SMALL,
+    MEDIUM,
+    BIG
+}
