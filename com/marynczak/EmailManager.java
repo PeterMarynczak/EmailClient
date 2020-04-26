02a18e51df48c660f4ff1d4136e0545ac39b6180
unknown <piotr.marynczak.programista@gmail.com>
2020-04-26 18:55:22 +0200

    added class to fetch folders from gmail

--- com/marynczak/EmailManager.java
+++ com/marynczak/EmailManager.java
@@ -1,24 +1,23 @@
 package com.marynczak;
 
+import com.marynczak.controller.services.FetchFoldersService;
 import com.marynczak.model.EmailAccount;
+import com.marynczak.model.EmailTreeItem;
 import javafx.scene.control.TreeItem;
 
 public class EmailManager {
 
     //Folder handling:
-    private TreeItem<String> foldersRoot = new TreeItem<String>("");
+    private EmailTreeItem<String> foldersRoot = new EmailTreeItem<>("");
 
-    public TreeItem<String> getFoldersRoot() {
+    public EmailTreeItem<String> getFoldersRoot() {
         return foldersRoot;
     }
 
     public void addEmailAccount(EmailAccount emailAccount){
-        TreeItem<String> treeItem = new TreeItem<String>(emailAccount.getAddress());
-        treeItem.setExpanded(true);
-        treeItem.getChildren().add(new TreeItem<String>("INBOX"));
-        treeItem.getChildren().add(new TreeItem<String>("Sent"));
-        treeItem.getChildren().add(new TreeItem<String>("Folder1"));
-        treeItem.getChildren().add(new TreeItem<String>("Spam"));
+        EmailTreeItem<String> treeItem = new EmailTreeItem<>(emailAccount.getAddress());
+        FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem);
+        fetchFoldersService.start();
         foldersRoot.getChildren().add(treeItem);
     }
 }
