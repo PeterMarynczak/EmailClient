842faa86bea494b90f193d39ae7694e80a1bf187
unknown <piotr.marynczak.programista@gmail.com>
2020-05-30 21:43:45 +0200

    interaction with emails - mark selected email as read

--- com/marynczak/EmailManager.java
+++ com/marynczak/EmailManager.java
@@ -3,14 +3,35 @@ package com.marynczak;
 import com.marynczak.controller.services.FetchFoldersService;
 import com.marynczak.controller.services.FolderUpdaterService;
 import com.marynczak.model.EmailAccount;
+import com.marynczak.model.EmailMessage;
 import com.marynczak.model.EmailTreeItem;
 import javafx.scene.control.TreeItem;
 
+import javax.mail.Flags;
 import javax.mail.Folder;
 import java.util.ArrayList;
 import java.util.List;
 
 public class EmailManager {
+
+    private EmailMessage selectedMessage;
+    private EmailTreeItem<String> selectedFolder;
+
+    public EmailMessage getSelectedMessage() {
+        return selectedMessage;
+    }
+
+    public void setSelectedMessage(EmailMessage selectedMessage) {
+        this.selectedMessage = selectedMessage;
+    }
+
+    public EmailTreeItem<String> getSelectedFolder() {
+        return selectedFolder;
+    }
+
+    public void setSelectedFolder(EmailTreeItem<String> selectedFolder) {
+        this.selectedFolder = selectedFolder;
+    }
 
     private FolderUpdaterService folderUpdaterService;
     //Folder handling:
@@ -35,5 +56,15 @@ public class EmailManager {
         FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem, folderList);
         fetchFoldersService.start();
         foldersRoot.getChildren().add(treeItem);
+    }
+
+    public void setRead() {
+        try {
+            selectedMessage.setRead(true);
+            selectedMessage.getMessage().setFlag(Flags.Flag.SEEN, true);
+            selectedFolder.decrementMessagesCount();
+        } catch (Exception e) {
+
+        }
     }
 }
