3f04faad2fee2de791a27ea986ebcf2b1e9ae238
unknown <piotr.marynczak.programista@gmail.com>
2020-05-30 20:55:19 +0200

    added message listener

--- com/marynczak/EmailManager.java
+++ com/marynczak/EmailManager.java
@@ -1,12 +1,18 @@
 package com.marynczak;
 
 import com.marynczak.controller.services.FetchFoldersService;
+import com.marynczak.controller.services.FolderUpdaterService;
 import com.marynczak.model.EmailAccount;
 import com.marynczak.model.EmailTreeItem;
 import javafx.scene.control.TreeItem;
 
+import javax.mail.Folder;
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmailManager {
 
+    private FolderUpdaterService folderUpdaterService;
     //Folder handling:
     private EmailTreeItem<String> foldersRoot = new EmailTreeItem<>("");
 
@@ -14,9 +20,19 @@ public class EmailManager {
         return foldersRoot;
     }
 
+    private List<Folder> folderList = new ArrayList<Folder>();
+    public List<Folder> getFolderList(){
+        return this.folderList;
+    }
+
+    public EmailManager(){
+    folderUpdaterService = new FolderUpdaterService(folderList);
+    folderUpdaterService.start();
+    }
+
     public void addEmailAccount(EmailAccount emailAccount){
         EmailTreeItem<String> treeItem = new EmailTreeItem<>(emailAccount.getAddress());
-        FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem);
+        FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem, folderList);
         fetchFoldersService.start();
         foldersRoot.getChildren().add(treeItem);
     }
