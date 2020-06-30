f38621f031e9203904943faac26fc849588c5ad5
unknown <piotr.marynczak.programista@gmail.com>
2020-06-30 10:54:25 +0200

    added class to save persisted data and load info from the file upon login

--- com/marynczak/Launcher.java
+++ com/marynczak/Launcher.java
@@ -1,9 +1,15 @@
 package com.marynczak;
 
 import com.marynczak.controller.persistence.PersistenceAccess;
+import com.marynczak.controller.persistence.ValidAccount;
+import com.marynczak.controller.services.LoginService;
+import com.marynczak.model.EmailAccount;
 import com.marynczak.view.ViewFactory;
 import javafx.application.Application;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Launcher extends Application {
 
@@ -16,8 +22,26 @@ public class Launcher extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        ViewFactory viewFactory = new ViewFactory(emailManager);
+        List<ValidAccount> validAccountList = persistenceAccess.loadFromPersistence();
+        if(validAccountList.size() > 0) {
+            viewFactory.showMainWindow();
+            for (ValidAccount validAccount: validAccountList){
+                EmailAccount emailAccount = new EmailAccount(validAccount.getAddress(), validAccount.getPassword());
+                LoginService loginService = new LoginService(emailAccount, emailManager);
+                loginService.start();
+            }
+        } else {
+            viewFactory.showLoginWindow();
+        }
+    }
 
-        ViewFactory viewFactory = new ViewFactory(new EmailManager());
-        viewFactory.showLoginWindow();
+    @Override
+    public void stop() throws Exception {
+        List<ValidAccount> validAccountList = new ArrayList<ValidAccount>();
+        for (EmailAccount emailAccount: emailManager.getEmailAccounts()){
+            validAccountList.add(new ValidAccount(emailAccount.getAddress(), emailAccount.getPassword()));
+        }
+        persistenceAccess.saveToPersistence(validAccountList);
     }
 }
