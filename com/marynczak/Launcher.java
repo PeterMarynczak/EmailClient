949ed0b9b7466fa06312fbf0c96ec002c6e98e6d
unknown <piotr.marynczak.programista@gmail.com>
2020-06-30 10:39:35 +0200

    added persistence and valid account class

--- com/marynczak/Launcher.java
+++ com/marynczak/Launcher.java
@@ -1,5 +1,6 @@
 package com.marynczak;
 
+import com.marynczak.controller.persistence.PersistenceAccess;
 import com.marynczak.view.ViewFactory;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -9,6 +10,9 @@ public class Launcher extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
+    private PersistenceAccess persistenceAccess = new PersistenceAccess();
+    private EmailManager emailManager = new EmailManager();
 
     @Override
     public void start(Stage stage) throws Exception {
